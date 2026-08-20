6f6b8aa35be6a7b814129cbb16584b1d4ec98d03
NootNootMan <surabya.satyal@ucas-edu.net>
2026-08-20 09:46:41 -0600

    Homework

---
+++ character_intro.py
@@ -0,0 +1,6 @@
+#Surabya Satyal 🔨 Character Introduction
+Name = " George Bobignton"
+Age = "67"
+Job = "President"
+Place = "Ohio"
+print("Hi, my name is" + Name+ ". I am " +Age+ " years old. I work as a " +Job+" and I am from " +Place+".")
