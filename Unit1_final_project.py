206d7db0df063316f5826b255eb904e9104ad5a2
NootNootMan <surabya.satyal@ucas-edu.net>
2026-08-26 09:42:43 -0600

    Final

---
+++ Unit1_final_project.py
@@ -0,0 +1,8 @@
+#Surabya Satyal Unit 1 final project
+name = input("What is your name?")
+age = input("How old are you?")
+place = input("What is your favourite place to visit?")
+color = input("Whats your favourite color?")
+meme = input("Whats your favourite meme?")
+
+print("Hello,I am " +name+ " nice to meet you.I am " +age+ " years old. My favourite color is " +color+ " and my favourite place is " +place+ ". I love watching " +meme+ ".") 
