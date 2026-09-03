bfb28a93316bb49466150f835a214d861af0f121
NootNootMan <surabya.satyal@ucas-edu.net>
2026-09-03 09:49:48 -0600

    NOOTNOOT

---
+++ madlib.py
@@ -0,0 +1,12 @@
+#Surabya Satyal Madlib
+name = input("Give me a name!")
+noun = input("Give me a noun!")
+verbing = input("Give me a verb ending with ing!")
+job = input("Give me a job!")
+adjective = input("Give me an adjective!")
+noun2 = input("Give me another noun!")
+verb = input("Give me a verb!")
+number = input("Give me a number!")
+name2 = input("Give me another name!")
+
+print("In 1981, the U.S. launched the first real space "+noun+"(noun). It was named "+name2+"(name2) and was piloted by a brave guy named " + name+"(name). They practiced " +verbing+ "(Verb with ing) for two years and were expert "+job+"(Job). It took off using  its powerful first-stage "+noun2+"(Noun2) and sores off the "+adjective+"(Adjective) blue "+noun+"(noun). At an alltitude of "+number+"feet,it orbited around the"+noun+"(Noun)for "+number+ "(Number)times.After that, it started going down and crashed.")
