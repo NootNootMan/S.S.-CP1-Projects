5838c0e462a3df67dac5e135a0f9bf1b0e643d75
NootNootMan <surabya.satyal@ucas-edu.net>
2026-09-08 09:22:59 -0600

    class

--- Practice.py
+++ Practice.py
@@ -1,4 +1,4 @@
-name1 = input("What is your first name: ").strip().title()
+"""name1 = input("What is your first name: ").strip().title()
 name2 = input("What is your last name: ").strip().title()
 seprated1 = name1.split()
 fixed = "".join(seprated1)
@@ -9,6 +9,14 @@ print("Hello "+full_name)#Comment
 
 print(full_name.isalpha())
 print(full_name.isnumeric())
-print(full_name.isupper())
+print(full_name.isupper())"""
+
+letter = input("Give me a letter :")
+letter = letter[0].lower()
+number_value = ord(letter)
+number_value += 2
+new_letter = chr(number_value)
+print(f"Your letter was {letter},now it is  {new_letter}")
+
 
 
