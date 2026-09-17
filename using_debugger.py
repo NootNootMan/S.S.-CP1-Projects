f48e464022a34808c1c67fc40baf71681b3c84b5
NootNootMan <surabya.satyal@ucas-edu.net>
2026-09-17 09:20:41 -0600

    hj

--- using_debugger.py
+++ using_debugger.py
@@ -5,17 +5,17 @@ pirate_name = input("What's your name, pirate? ")
 snack_name = input("What snack do you want? ")
 
 price = random.randint(2, 8)  # random price in credits
-quantity = input("How many would you like? ")
+quantity = int(input("How many would you like? "))#Made it an integer
 
 total = price * quantity
 
-discounted_total = total - 2 * 0.10
+discounted_total = total - (total * 0.10)#Changed 2 to total
 
 tax_rate = 0.08
 total_with_tax = discounted_total + (discounted_total * tax_rate)
 
 print("Hello, " + pirate_name + "! Here's your order summary:")
-print("Snack: " + snackName)
+print("Snack: " + snack_name)#It was snackName. So I changed it to snack_name.
 print("Price per snack: " + str(price) + " credits")
-print("Total before tax: " + str(price))
-print("Total with tax: " + str(round(total_with_tax, 2)) + " credits"   
+print("Discounted Total before tax: " + str(discounted_total))# I changed from price to discounted total.
+print("Total with tax: " + str(round(total_with_tax, 2)) + " credits")#The bracket wasnt closed. So I closed it.   
