89c6f34171777980980fad63a74fb76aba7c46ac
NootNootMan <surabya.satyal@ucas-edu.net>
2026-09-18 09:21:59 -0600

    sdDsddSDdsds

--- using_debugger.py
+++ using_debugger.py
@@ -17,5 +17,5 @@ total_with_tax = discounted_total + (discounted_total * tax_rate)
 print("Hello, " + pirate_name + "! Here's your order summary:")
 print("Snack: " + snack_name)#It was snackName. So I changed it to snack_name.
 print("Price per snack: " + str(price) + " credits")
-print("Discounted Total before tax: " + str(discounted_total))# I changed from price to discounted total.
+print(" Total before tax: " + str(total))# I changed from price to total.
 print("Total with tax: " + str(round(total_with_tax, 2)) + " credits")#The bracket wasnt closed. So I closed it.   
