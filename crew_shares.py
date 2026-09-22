e5942154a7b2fd999b8af18a2083f4d7adf0441a
NootNootMan <surabya.satyal@ucas-edu.net>
2026-09-22 10:44:50 -0600

    Update crew_shares.py
    lllll

--- crew_shares.py
+++ crew_shares.py
@@ -16,12 +16,12 @@ print(f"So, in total you have {pirate_number} pirates")
 print("Yondu Udonta and his crew arrive at the Iron Lotus after several weeks of plundering various places around the galaxy. The crew has been in space for nearly six months and they are ready for a night of celebration. Yondu doesn't want to divvy up the plunder just yet, so he gives each crew member other than himself and Peter Quill 3 units and sends them off to the Iron Lotus. After the crew has gone, he and Peter count what's left and decide how to split it up among the crew. Yondu takes 13% of the total. He then gives Peter 11% of what's left. The next morning, Yondu divides the remaining amount evenly among all of the crew, including Yondu and Quill. The crew does not know that Yondu and Quill have already taken a cut.")
 
 treasure_got = random.randint(500,5000)
-print(f"They have {treasure_got} units from the treasure."")
+print(f"They have {treasure_got} units from the treasure.")
 
 pirate_away = int(pirate_number) - 2
 
 money_units = treasure_got - (int(pirate_away)*3)
-print(f"As Yondu and Quill gave 3 units to each away member, the total treasure left is now {money_units:.2f}.")
+print(f"As Yondo and Quill gave 3 units to each away member, the total treasure left is now {money_units:.2f}.")
 
 
 thief_yondo = (13 / 100) * money_units
