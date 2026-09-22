95923a103b424be737568eb6e9a85d53cb7509b1
NootNootMan <surabya.satyal@ucas-edu.net>
2026-09-22 10:12:44 -0600

    Update crew_shares.py
    lol

--- crew_shares.py
+++ crew_shares.py
@@ -3,7 +3,7 @@ import random
 
 while True:
     try:
-        pirate_number = input("How many pirates do you have, including Quill and Yondo?")
+        pirate_number = input("How many pirates do you have including Quill and Yondo?")
         if not pirate_number.isnumeric():
             print("Put a valid number")
             continue
@@ -17,8 +17,8 @@ while True:
 
 treasure_got = random.randint(500,5000)
 print("They have "+str(treasure_got)+" units from the treasure.")
-
-money_units = treasure_got -int(pirate_number)*3
+pirates_away = (pirates_number) - 2
+money_units = treasure_got -int(pirate_away)*3
 print("As Yondo and Quill gave 3 units to each member, there total treasure is now "+str(money_units)+".")
 
 
@@ -30,12 +30,12 @@ print("Now as the team was gone for the night, Quill also came and took 11% of t
 
 remaining_treasure = money_units - thief_yondo - thief_quill
 print("The day of splitting has arrived!!!")
-money_for_all = round(((remaining_treasure) / (pirate_number)) , 2)
+money_for_all = round(((remaining_treasure) / int(pirate_number)) , 2)
 
 print("Everyone in the crew got "+(money_for_all)+" units.")
 
 real_yondo = (money_for_all) + (thief_yondo)
-print("Yondo got "+real_yondo+ "units.")
+print("Yondo got "+str(real_yondo)+ "units.")
 
 real_quill = (money_for_all) + (thief_quill)
-print("Quill got "+real_quill+ "units.")
+print("Quill got "+str(real_quill)+ "units.")
