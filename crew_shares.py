17e193685c6cfc2544f33d4f689070f25650fca5
NootNootMan <surabya.satyal@ucas-edu.net>
2026-09-22 10:26:05 -0600

    Update crew_shares.py
    lolollol

--- crew_shares.py
+++ crew_shares.py
@@ -12,6 +12,7 @@ while True:
         print("Invalid input.")
 
 print("So, in total you have "+pirate_number+" pirates")
+
 print("Yondu Udonta and his crew arrive at the Iron Lotus after several weeks of plundering various places around the galaxy. The crew has been in space for nearly six months and they are ready for a night of celebration. Yondu doesn't want to divvy up the plunder just yet, so he gives each crew member other than himself and Peter Quill 3 units and sends them off to the Iron Lotus. After the crew has gone, he and Peter count what's left and decide how to split it up among the crew. Yondu takes 13% of the total. He then gives Peter 11% of what's left. The next morning, Yondu divides the remaining amount evenly among all of the crew, including Yondu and Quill. The crew does not know that Yondu and Quill have already taken a cut.")
 
 treasure_got = random.randint(500,5000)
