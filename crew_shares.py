a24672e7f72d196955975adccfffd182223d71b0
NootNootMan <surabya.satyal@ucas-edu.net>
2026-09-22 09:38:17 -0600

    fh

--- crew_shares.py
+++ crew_shares.py
@@ -19,4 +19,17 @@ treasure_got = random.randint(500,5000)
 print("They have "+str(treasure_got)+" units from the treasure.")
 
 money_units = treasure_got -int(pirate_number)*3
-print("As Yondo and Quill gave 3 units to each member, there total treasure is now"+str(money_units)+".")
+print("As Yondo and Quill gave 3 units to each member, there total treasure is now "+str(money_units)+".")
+
+
+thief_yondo = round(13/100 *(money_units),2)
+print("Now as the team was gone for the night, Yondu came and took 13% of the treasure which was a whopping "+str(thief_yondo)+" Units.")
+
+thief_quill = round(11/100 *(money_units - thief_yondo),2)
+print("Now as the team was gone for the night, Quill also came and took 11% of the treasure which was a whopping "+str(thief_quill)+" Units.")
+
+remaining_treasure = money_units - thief_yondo - thief_quill
+print("The day of splitting has arrived!!!")
+money_for_all = round(((remaining_treasure) / (pirate_number)) , 2)
+
+print("Everyone in the crew got "+(money_for_all)+" units.")
