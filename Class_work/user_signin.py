d8c3554e068d264b31c12201f02be03e2a73b298
NOOTNOOTMAN <surabya.satyal@ucas-edu.net>
2026-09-25 09:09:42 -0600

    xvcxvx

--- Class_work/user_signin.py
+++ Class_work/user_signin.py
@@ -4,7 +4,15 @@ username = input("Write the username: ")
 password = input("Enter the password: ")
 
 if username == ('Istealch33se'):
-    print("Correct username")
+    print("Correct username.")
 else:
-    print("Incorrect username")
+    print("Incorrect username.")
     
+if password == ('t3isisn0tmypa44word'):
+    print('Correct password.') 
+else:
+    print("Incorrect password.")
+
+
+if username == ('Istealch33se') and password == ('t3isisn0tmypa44word'):
+    print("Welcome User.Everything is loading please wait...")
